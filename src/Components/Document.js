7696b051a5b2722ec7b304384dc6eb72e1797c79
SIwanont Sittinam <siwanont1@gmail.com>
2019-04-24 02:40:50 +0700

    fix path

--- src/Components/Document.js
+++ src/Components/Document.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 
 class Document extends Component {
-  render() {
+   render() {
+      const storagepath = "https://public.siwanont.ml/XuHyresj35jIOyPma7CGyg/";
 
-    if(this.props.data){
-      var networks = this.props.data.download.map(function (network) {
-         return <li key={network.name}><a href={"documents/" + network.url} className="button"><i className="fa fa-download"></i> Download {network.name}</a></li>
-      })
-    }
+      if(this.props.data){
+         var networks = this.props.data.download.map(function (network) {
+            return <li key={network.name}><a href={storagepath + network.url} className="button"><i className="fa fa-download"></i> Download {network.name}</a></li>
+         })
+      }
 
-    return (
-      <section id="document">
-         <div className="row">
-            <div className="three columns"></div>
-            <div className="nine columns main-col">
-               <h2>Documents</h2>
-               <ul className="download">
-                  {networks}
-               </ul>
+      return (
+         <section id="document">
+            <div className="row">
+               <div className="three columns"></div>
+               <div className="nine columns main-col">
+                  <h2>Documents</h2>
+                  <ul className="download">
+                     {networks}
+                  </ul>
+               </div>
             </div>
-         </div>
-      </section>
-    );
-  }
+         </section>
+      );
+   }
 }
 
 export default Document;
