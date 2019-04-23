0269b2a5a5c112c830309947eadcc2fc20053fc0
SIwanont Sittinam <siwanont1@gmail.com>
2019-04-24 03:01:01 +0700

    fix path

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,58 +1,61 @@
 import React, { Component } from 'react';
 
 class About extends Component {
-  render() {
+   render() {
+      const storagepath = "https://public.siwanont.ml/" + "XuHyresj35jIOyPma7CGyg/";
+      const cdnpath = "https://public.siwanont.ml/" + "GRHVkN5NwxcGmHUXsMOu3Q/";
+      // const storagepath = "documents/";
 
-    if(this.props.data){
-      var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
-      var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var resumeName = this.props.data.download[0].name;
-      var resumeDownload = "documents/" + this.props.data.download[0].url;
-    }
+      if (this.props.data) {
+         var name = this.props.data.name;
+         var profilepic = cdnpath + "image/" + this.props.data.image;
+         var bio = this.props.data.bio;
+         var street = this.props.data.address.street;
+         var city = this.props.data.address.city;
+         var state = this.props.data.address.state;
+         var zip = this.props.data.address.zip;
+         var phone = this.props.data.phone;
+         var email = this.props.data.email;
+         var resumeName = this.props.data.download[0].name;
+         var resumeDownload = storagepath + this.props.data.download[0].url;
+      }
 
-    return (
-      <section id="about">
-      <div className="row">
-         <div className="three columns">
-            <img className="profile-pic" src={profilepic} alt="Siwanont Sittinam Profile Pic" />
-         </div>
-         <div className="nine columns main-col">
-            <h2>About Me</h2>
-            <p>{bio}</p>
+      return (
+         <section id="about">
             <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city}, {state}, <br/> {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
-                     <span>{email}</span>
-					   </p>
+               <div className="three columns">
+                  <img className="profile-pic" src={profilepic} alt="Siwanont Sittinam Profile Pic" />
                </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download {resumeName}</a>
-                  </p>
-                  {/* <p>
+               <div className="nine columns main-col">
+                  <h2>About Me</h2>
+                  <p>{bio}</p>
+                  <div className="row">
+                     <div className="columns contact-details">
+                        <h2>Contact Details</h2>
+                        <p className="address">
+                           <span>{name}</span><br />
+                           <span>{street}<br />
+                              {city}, {state}, <br /> {zip}
+                           </span><br />
+                           <span>{phone}</span><br />
+                           <span>{email}</span>
+                        </p>
+                     </div>
+                     <div className="columns download">
+                        <p>
+                           <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download {resumeName}</a>
+                        </p>
+                        {/* <p>
                      <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download {resumeName}</a>
                   </p> */}
+                     </div>
+                  </div>
                </div>
             </div>
-         </div>
-      </div>
 
-   </section>
-    );
-  }
+         </section>
+      );
+   }
 }
 
 export default About;
