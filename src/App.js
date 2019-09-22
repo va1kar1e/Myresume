24226e5801e96993c5230687b5fd7b73bad248ed
Siwanont Sittinam <siwanont1@gmail.com>
2019-09-22 22:36:26 +0700

    Try App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./assets/scss/material-kit-react.scss?v=1.8.0";
 
 // pages for this product
 // import Components from "views/Components/Components.js";
-// import LandingPage from "views/LandingPage/LandingPage.js";
+import LandingPage from "./views/LandingPage/LandingPage.js";
 // import ProfilePage from "views/ProfilePage/ProfilePage.js";
 // import LoginPage from "views/LoginPage/LoginPage.js";
 
@@ -17,10 +17,10 @@ function App() {
   return (
     <Router history={hist}>
       <Switch>
-        {/* <Route path="/landing-page" component={LandingPage} />
-        <Route path="/profile-page" component={ProfilePage} />
-        <Route path="/login-page" component={LoginPage} />
-        <Route path="/" component={Components} /> */}
+        <Route path="/landing-page" component={LandingPage} />
+        {/* <Route path="/profile-page" component={ProfilePage} /> */}
+        {/* <Route path="/login-page" component={LoginPage} /> */}
+        {/* <Route path="/" component={Components} /> */}
       </Switch>
     </Router>
   );
