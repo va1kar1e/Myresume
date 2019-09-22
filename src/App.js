d84dbc61fa2f6dd85503a581dea48377204d14dc
Siwanont Sittinam <siwanont1@gmail.com>
2019-09-22 22:09:23 +0700

    Add Module SCSS

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+
 import './App.css';
+import "./assets/scss/material-kit-react.scss?v=1.8.0";
+
+// pages for this product
+// import Components from "views/Components/Components.js";
+// import LandingPage from "views/LandingPage/LandingPage.js";
+// import ProfilePage from "views/ProfilePage/ProfilePage.js";
+// import LoginPage from "views/LoginPage/LoginPage.js";
+
+var hist = createBrowserHistory();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Siwanont Sittinam [V2]
-        </p>
-      </header>
-    </div>
+    <Router history={hist}>
+      <Switch>
+        {/* <Route path="/landing-page" component={LandingPage} />
+        <Route path="/profile-page" component={ProfilePage} />
+        <Route path="/login-page" component={LoginPage} />
+        <Route path="/" component={Components} /> */}
+      </Switch>
+    </Router>
   );
 }
 
