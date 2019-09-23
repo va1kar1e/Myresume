d35a881def06d6765f21118a54f83b9842f9107e
Siwanont Sittinam <siwanont1@gmail.com>
2019-09-23 22:14:02 +0700

    Modified Experience

---
+++ src/pages/Experience/ExperienceStyle.js
@@ -0,0 +1,13 @@
+import { container, title } from "../../components/material-kit-react"
+
+const ExperienceStyle = {
+    container,
+    title: {
+        ...title,
+        marginTop: "30px",
+        minHeight: "32px",
+        textDecoration: "none"
+    }
+};
+
+export default ExperienceStyle;
---
+++ src/pages/Experience/index.js
@@ -0,0 +1,91 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// @material-ui/icons
+import Dashboard from "@material-ui/icons/Dashboard";
+import Schedule from "@material-ui/icons/Schedule";
+
+// core components
+import GridContainer from "../../components/Grid/GridContainer";
+import GridItem from "../../components/Grid/GridItem";
+import NavPills from "../../components/NavPills";
+
+import styles from "./ExperienceStyle";
+
+const useStyles = makeStyles(styles);
+
+export default function Experience() {
+  const classes = useStyles();
+  return (
+    <div id="experience">
+        <div className={classes.title}>
+            <h3>Experience</h3>
+        </div>
+        <GridContainer>
+        <GridItem xs={12} sm={12} md={12} lg={12}>
+            <NavPills
+            color="rose"
+            horizontal={{
+                tabsGrid: { xs: 12, sm: 2, md: 2 },
+                contentGrid: { xs: 12, sm: 10, md: 10 }
+            }}
+            tabs={[
+                {
+                tabButton: "Dashboard",
+                tabIcon: Dashboard,
+                tabContent: (
+                    <span>
+                    <p>
+                        Collaboratively administrate empowered markets via
+                        plug-and-play networks. Dynamically procrastinate B2C
+                        users after installed base benefits.
+                    </p>
+                    <br />
+                    <p>
+                        Dramatically visualize customer directed convergence
+                        without revolutionary ROI. Collaboratively
+                        administrate empowered markets via plug-and-play
+                        networks. Dynamically procrastinate B2C users after
+                        installed base benefits.
+                    </p>
+                    <br />
+                    <p>
+                        Dramatically visualize customer directed convergence
+                        without revolutionary ROI. Collaboratively
+                        administrate empowered markets via plug-and-play
+                        networks. Dynamically procrastinate B2C users after
+                        installed base benefits.
+                    </p>
+                    </span>
+                )
+                },
+                {
+                tabButton: "Schedule",
+                tabIcon: Schedule,
+                tabContent: (
+                    <span>
+                    <p>
+                        Efficiently unleash cross-media information without
+                        cross-media value. Quickly maximize timely
+                        deliverables for real-time schemas.
+                    </p>
+                    <br />
+                    <p>
+                        Dramatically maintain clicks-and-mortar solutions
+                        without functional solutions. Dramatically visualize
+                        customer directed convergence without revolutionary
+                        ROI. Collaboratively administrate empowered markets
+                        via plug-and-play networks. Dynamically procrastinate
+                        B2C users after installed base benefits.
+                    </p>
+                    </span>
+                )
+                }
+            ]}
+            />
+        </GridItem>
+        </GridContainer>
+    </div>
+  );
+}
