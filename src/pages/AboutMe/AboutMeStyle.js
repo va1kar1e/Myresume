84c95b2472174b1b00c2065ec3813b28319b8b37
Siwanont Sittinam <siwanont1@gmail.com>
2019-09-23 22:41:02 +0700

    Modified Aboutme

--- src/pages/AboutMe/AboutMeStyle.js
+++ src/pages/AboutMe/AboutMeStyle.js
@@ -1,12 +1,22 @@
 import { container, title } from "../../components/material-kit-react"
 
 const ExperienceStyle = {
-    container,
     title: {
         ...title,
         marginTop: "30px",
         minHeight: "32px",
         textDecoration: "none"
+    },
+    section: {
+        padding: "70px 0"
+    },
+    container: {
+        ...container,
+        textAlign: "center !important"
+    },
+    marginAuto: {
+        marginLeft: "auto !important",
+        marginRight: "auto !important"
     }
 };
 
--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -1,96 +1,88 @@
 import React from "react";
+// react component for creating beautiful carousel
+import Carousel from "react-slick";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-
 // @material-ui/icons
-import Dashboard from "@material-ui/icons/Dashboard";
-import Schedule from "@material-ui/icons/Schedule";
-
+import LocationOn from "@material-ui/icons/LocationOn";
 // core components
 import GridContainer from "../../components/Grid/GridContainer";
 import GridItem from "../../components/Grid/GridItem";
-import NavPills from "../../components/NavPills";
+import Card from "../../components/Card/Card";
 
 import styles from "./AboutMeStyle";
 
 const useStyles = makeStyles(styles);
 
+const   image1 = "",
+        image2 = "",
+        image3 = "";
+
 export default function Aboutme() {
   const classes = useStyles();
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false
+  };
   return (
       <div id="aboutme">
           <div className={classes.title}>
               <h3>About Me</h3>
           </div>
-          <div className={classes.title}>
-              <h3>
-                  <small>With Icons</small>
-              </h3>
-          </div>
-          <GridContainer>
-              <GridItem xs={12} sm={12} md={12} lg={12}>
-                  <NavPills
-                      color="rose"
-                      horizontal={{
-                          tabsGrid: { xs: 12, sm: 2, md: 2 },
-                          contentGrid: { xs: 12, sm: 10, md: 10 }
-                      }}
-                      tabs={[
-                          {
-                              tabButton: "Dashboard",
-                              tabIcon: Dashboard,
-                              tabContent: (
-                                  <span>
-                                      <p>
-                                          Collaboratively administrate empowered markets via
-                                          plug-and-play networks. Dynamically procrastinate B2C
-                                          users after installed base benefits.
-                    </p>
-                                      <br />
-                                      <p>
-                                          Dramatically visualize customer directed convergence
-                                          without revolutionary ROI. Collaboratively
-                                          administrate empowered markets via plug-and-play
-                                          networks. Dynamically procrastinate B2C users after
-                                          installed base benefits.
-                    </p>
-                                      <br />
-                                      <p>
-                                          Dramatically visualize customer directed convergence
-                                          without revolutionary ROI. Collaboratively
-                                          administrate empowered markets via plug-and-play
-                                          networks. Dynamically procrastinate B2C users after
-                                          installed base benefits.
-                    </p>
-                                  </span>
-                              )
-                          },
-                          {
-                              tabButton: "Schedule",
-                              tabIcon: Schedule,
-                              tabContent: (
-                                  <span>
-                                      <p>
-                                          Efficiently unleash cross-media information without
-                                          cross-media value. Quickly maximize timely
-                                          deliverables for real-time schemas.
-                    </p>
-                                      <br />
-                                      <p>
-                                          Dramatically maintain clicks-and-mortar solutions
-                                          without functional solutions. Dramatically visualize
-                                          customer directed convergence without revolutionary
-                                          ROI. Collaboratively administrate empowered markets
-                                          via plug-and-play networks. Dynamically procrastinate
-                                          B2C users after installed base benefits.
-                    </p>
-                                  </span>
-                              )
-                          }
-                      ]}
-                  />
-              </GridItem>
-          </GridContainer>
+            <GridContainer>
+                <GridItem xs={12} sm={12} md={8} className={classes.marginAuto}>
+                    <Card carousel>
+                    <Carousel {...settings}>
+                        <div>
+                        <img src={image1} alt="First slide" className="slick-image" />
+                        <div className="slick-caption">
+                            <h4>
+                            <LocationOn className="slick-icons" />
+                            Yellowstone National Park, United States
+                            </h4>
+                        </div>
+                        </div>
+                        <div>
+                        <img
+                            src={image2}
+                            alt="Second slide"
+                            className="slick-image"
+                        />
+                        <div className="slick-caption">
+                            <h4>
+                            <LocationOn className="slick-icons" />
+                            Somewhere Beyond, United States
+                            </h4>
+                        </div>
+                        </div>
+                        <div>
+                        <img src={image3} alt="Third slide" className="slick-image" />
+                        <div className="slick-caption">
+                            <h4>
+                            <LocationOn className="slick-icons" />
+                            Yellowstone National Park, United States
+                            </h4>
+                        </div>
+                        </div>
+                    </Carousel>
+                    </Card>
+                </GridItem>
+            </GridContainer>
+            <GridContainer justify="center">
+                <GridItem xs={12} sm={12} md={8}>
+                    <h2>Completed with examples</h2>
+                    <h4>
+                    The kit comes with three pre-built pages to help you get started
+                    faster. You can change the text and images and you{"'"}re good to
+                    go. More importantly, looking at them will give you a picture of
+                    what you can build with this powerful kit.
+                    </h4>
+                </GridItem>
+            </GridContainer>
       </div>
   );
 }
