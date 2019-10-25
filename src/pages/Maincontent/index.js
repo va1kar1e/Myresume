d7326fe61823be2cfe1c70adc30db50ea8abf185
Siwanont Sittinam <siwanont1@gmail.com>
2019-10-25 23:11:17 +0700

    Reinit

--- src/pages/Maincontent/index.js
+++ src/pages/Maincontent/index.js
@@ -2,7 +2,7 @@ import React, { useReducer, useEffect } from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import classNames from "classnames";
 // core components
-// import firebase from "../../firebaseConfig";
+import firebase from "../../firebaseConfig";
 // import Header from "../../components/Header";
 // import GridContainer from "../../components/Grid/GridContainer";
 // import GridItem from "../../components/Grid/GridItem";
@@ -13,44 +13,45 @@ import classNames from "classnames";
 // import AboutMe from "../AboutMe";
 // import Experience from "../Experience";
 
-// import styles from "./mainContentStyle";
-// const useStyles = makeStyles(styles);
+import styles from "./mainContentStyle";
+const useStyles = makeStyles(styles);
 
 // const dbCollection = firebase.firestore().collection("XuHyresj35jIOyPma7CGyg");
 // const assetsStorage = firebase.storage().ref().child('assets');
 
-// const initialState = {
-//     isFetching: false,
-//     content: {},
-//     link: {}
-// }
+const initialState = {
+    isFetching: false,
+    content: {},
+    link: {}
+}
 
-// const reducer = (state, { type, link, content }) => {
-//     switch (type) {
-//         case 'FETCH_PENDING':
-//             return {
-//                 ...state,
-//                 isFetching: true
-//             }
-//         case 'FETCH_SUCCESS':
-//             return {
-//                 ...state,
-//                 isFetching: false,
-//                 link: link,
-//                 content: content
-//             }
-//         default:
-//             return state
-//     }
-// }
+const reducer = (state, { type, link, content }) => {
+    switch (type) {
+        case 'FETCH_PENDING':
+            return {
+                ...state,
+                isFetching: true
+            }
+        case 'FETCH_SUCCESS':
+            return {
+                ...state,
+                isFetching: false,
+                link: link,
+                content: content
+            }
+        default:
+            return state
+    }
+}
 
 function MainContent(props) {
     
     const classes = useStyles();
     const { ...rest } = props;
     
-    // const [{ content, link, isFetching }, dispatch] = useReducer(reducer, initialState);
-    // const imgpath = assetsStorage.child(link.headerbg).fullPath;
+    const [{ content, link, isFetching }, dispatch] = useReducer(reducer, initialState);
+    const imgpath = "";
+    // assetsStorage.child(link.headerbg).fullPath;
     
     // useEffect(() => {
     //     dispatch({
@@ -87,6 +88,7 @@ function MainContent(props) {
     // console.log()
     return (
         <div>
+            {imgpath}
             {/* <Header
                 brand={content.headername}
                 rightLinks={<HeaderLinks />}
