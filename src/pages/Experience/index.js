3ea9298928ec9147f3bc0d46b33cb7f04ec2dd33
Siwanont Sittinam <siwanont1@gmail.com>
2019-11-02 04:08:11 +0700

    Update index.js

--- src/pages/Experience/index.js
+++ src/pages/Experience/index.js
@@ -3,28 +3,65 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
 // @material-ui/icons
-import School from "@material-ui/icons/School";
-import Work from "@material-ui/icons/Work";
+import SchoolIcon from "@material-ui/icons/School";
+import WorkIcon from "@material-ui/icons/Work";
+import StarIcon from "@material-ui/icons/Star";
 
 // core components
 import GridContainer from "../../components/Grid/GridContainer";
 import GridItem from "../../components/Grid/GridItem";
 import NavPills from "../../components/NavPills";
 
+// Timeline Component
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+
 import styles from "./ExperienceStyle";
 
 const useStyles = makeStyles(styles);
 
 export default function Experience() {
-  const classes = useStyles();
-  return (
-    <div id="experience">
-        <div className={classes.title}>
-            <h2>Experience</h2>
-        </div>
-        <GridContainer>
-        <GridItem xs={12} sm={12} md={12} lg={12}>
-            <NavPills
+    const classes = useStyles();
+    return (
+        <div id="experience">
+            <div className={classes.title}>
+                <h2>Experience</h2>
+            </div>
+            <GridContainer>
+                <GridItem xs={12} sm={12} md={12} lg={12}>
+                    <VerticalTimeline>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                            date="2011 - present"
+                            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                            icon={<WorkIcon />}
+                        >
+                            <h3 className="vertical-timeline-element-title">Creative Director</h3>
+                            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+                            <p>
+                                Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+    </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--education"
+                            date="April 2013"
+                            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+                            icon={<SchoolIcon />}
+                        >
+                            <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
+                            <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+                            <p>
+                                Strategy, Social Media
+    </p>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+                            icon={<StarIcon />}
+                        />
+                    </VerticalTimeline>
+
+                    {/* <NavPills
             color="info"
             horizontal={{
                 tabsGrid: { xs: 12, sm: 2, md: 2 },
@@ -137,9 +174,9 @@ export default function Experience() {
                     )
                 }
             ]}
-            />
-        </GridItem>
-        </GridContainer>
-    </div>
-  );
+            />*/}
+                </GridItem>
+            </GridContainer>
+        </div>
+    );
 }
