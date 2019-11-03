f3e5a66ccfe7ae952075c53e919a034eb670ca86
Siwanont Sittinam <siwanont1@gmail.com>
2019-11-03 23:34:49 +0700

    Update index.js

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -1,17 +1,18 @@
 import React, { Fragment } from "react";
 // react component for creating beautiful carousel
-import Carousel from "react-slick";
+// import Carousel from "react-slick";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
+
 // @material-ui/icons
-import LocationOn from "@material-ui/icons/LocationOn";
+// import LocationOn from "@material-ui/icons/LocationOn";
 // core components
 import GridContainer from "../../components/Grid/GridContainer";
 import GridItem from "../../components/Grid/GridItem";
-import Card from "../../components/Card/Card";
+// import Card from "../../components/Card/Card";
+import ImageButton from "../../components/ImageButton";
 
 import styles from "./AboutMeStyle";
-
 const useStyles = makeStyles(styles);
 
 const getVal = (nestedObj, pathArr) => {
@@ -23,14 +24,14 @@ export default function Aboutme(props) {
     const classes = useStyles();
     const { content } = props;
 
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true
-    };
+    // const settings = {
+    //     dots: true,
+    //     infinite: true,
+    //     speed: 500,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     autoplay: true
+    // };
 
     const images = getVal(content, ['image']);
 
@@ -44,9 +45,10 @@ export default function Aboutme(props) {
                     </h4>
                 </GridItem>
             </GridContainer>
-            <GridContainer>
+            <GridContainer justify="center">
                 <GridItem xs={12} sm={12} md={10} className={classes.marginAuto}>
-                    <Card carousel>
+                    {images ? <ImageButton title="Hire Me" images={images[0]} /> : <Fragment></Fragment>}
+                    {/* <Card carousel>
                         <Carousel {...settings}>
                             {images ? images.map((image, index) => (
                                 <Fragment key={index}>
@@ -60,7 +62,7 @@ export default function Aboutme(props) {
                                 </Fragment>
                             )) : <Fragment></Fragment> }
                         </Carousel>
-                    </Card>
+                    </Card> */}
                 </GridItem>
             </GridContainer>
         </div>
