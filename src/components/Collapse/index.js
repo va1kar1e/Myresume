3171639061b3f0735f866e94168f06d77403563b
Siwanont Sittinam <siwanont1@gmail.com>
2019-11-09 14:19:06 +0700

    Update index.js

--- src/components/Collapse/index.js
+++ src/components/Collapse/index.js
@@ -1,14 +1,21 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import CardMedia from '@material-ui/core/CardMedia';
 import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles(theme => ({
+    media: {
+        height: 140,
+    },
     expand: {
+        float: "right",
         transform: 'rotate(0deg)',
         marginLeft: 'auto',
         transition: theme.transitions.create('transform', {
@@ -28,24 +35,43 @@ export default function RecipeReviewCard(props) {
     const classes = useStyles();
     const [expanded, setExpanded] = React.useState(false);
 
-    const {content} = props;
+    const { color, image, title, subtitle, content} = props;
      
     const handleExpandClick = () => {
         setExpanded(!expanded);
     };
 
     return (
-        <Fragment>
-            <IconButton
-                className={clsx(classes.expand, {
-                    [classes.expandOpen]: expanded,
-                })}
-                onClick={handleExpandClick}
-                aria-expanded={expanded}
-                aria-label="show more"
-            >
-                <ExpandMoreIcon />
-            </IconButton>
+        <Card className={classes.card} style={{ background:color}}>
+            <CardMedia
+                component="img"
+                className={classes.media}
+                image={image}
+                title={title}
+            />
+            <CardContent>
+                <Typography gutterBottom variant="h5">
+                    {title}
+                </Typography>
+                <Typography variant="h6" color="textSecondary">
+                    {subtitle}
+                </Typography>
+                <Typography variant="body2" component="p">
+                    {subtitle}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <IconButton
+                    className={clsx(classes.expand, {
+                        [classes.expandOpen]: expanded,
+                    })}
+                    onClick={handleExpandClick}
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </IconButton>
+            </CardActions>
             <Collapse in={expanded} timeout="auto" unmountOnExit>
                 <CardContent>
                     <Typography paragraph className={classes.contentTimeline}>
@@ -53,6 +79,6 @@ export default function RecipeReviewCard(props) {
                     </Typography>
                 </CardContent>
             </Collapse>
-        </Fragment>
+        </Card>
     );
 }
