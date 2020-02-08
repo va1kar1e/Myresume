6ee34c9041aced2be64124af016f74a2237701da
Siwanont Sittinam <siwanont1@gmail.com>
2020-02-08 18:59:45 +0700

    Add Hireme

---
+++ src/views/Hireme.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hireme">
+    <el-button type="text" @click="dialogFormVisible = true">
+      Hire Me
+    </el-button>
+    <el-dialog title="Hire Me" :visible.sync="dialogFormVisible">
+      <el-form :model="form">
+        <el-form-item label="Name" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="Email" :label-width="formLabelWidth">
+          <el-input v-model="form.email" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="Text" :label-width="formLabelWidth">
+          <el-input v-model="form.content" autocomplete="off" type="textarea" />
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">
+          Cancel
+        </el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">
+          Confirm
+        </el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialogTableVisible: false,
+      dialogFormVisible: false,
+      form: {
+        name: "",
+        email: "",
+        content: ""
+      },
+      formLabelWidth: "120px"
+    };
+  }
+};
+</script>
