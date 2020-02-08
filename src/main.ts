cde215a6a4b4eee234d5dd41a821ee6601ec3e13
Siwanont Sittinam <siwanont1@gmail.com>
2020-02-08 10:31:24 +0700

    Init Element-UI

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import "@/styles/index.scss";
+import locale from "element-ui/lib/locale/lang/en";
 
+Vue.use(ElementUI, { locale });
 Vue.config.productionTip = false;
 
 new Vue({
