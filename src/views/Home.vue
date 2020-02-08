0525268ab3d5e1cfe57cef177d0f489105cb34a3
Siwanont Sittinam <siwanont1@gmail.com>
2020-02-08 22:41:40 +0700

    Add responsive condition

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,22 @@
   <div class="home content-container">
     <h1>{{ title }}</h1>
     <Hireme />
+    <v-show-at small> 😸 </v-show-at>
+    <v-show-at medium> 😺 </v-show-at>
+    <v-show-at large> 😽 </v-show-at>
+    <v-show-at xlarge> 🐱 </v-show-at>
+    <v-show-at no-match> 😿 </v-show-at>
   </div>
 </template>
 
 <script>
 import Hireme from "@/views/Hireme.vue";
+import { VShowAt } from "vue-breakpoint-component";
 
 export default {
   components: {
-    Hireme
+    Hireme,
+    VShowAt
   },
   data() {
     return {
