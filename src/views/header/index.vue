dc226ff63ac87e8da23e002722ab8ceb0a617105
Siwanont Sittinam <siwanont1@gmail.com>
2020-02-10 23:45:06 +0700

    Add XS content

--- src/views/header/index.vue
+++ src/views/header/index.vue
@@ -7,11 +7,11 @@
         <div class="content-title">
           <p class="p-header-name-xl">
             Hi, My nickname is Books. I am a dedicated, hardworking, and
-            proactive Network Engineer. I am interested in <b>Networking</b>,
-            <b>Cybersecurity</b>, and <b>Data Analytics</b>. I constantly seek
-            new knowledge and actively develop new skils to improve myself. I
-            seek chalenging opportunities where I can fully use my skills for
-            the success of the organization.
+            proactive Network Engineer. I am interested in
+            <b>Networking</b>,<b>Cybersecurity</b>, and <b>Data Analytics</b>. I
+            constantly seek new knowledge and actively develop new skils to
+            improve myself. I seek chalenging opportunities where I can fully
+            use my skills for the success of the organization.
           </p>
         </div>
       </el-col>
@@ -58,11 +58,11 @@
         <div class="content-title">
           <p class="p-header-name-lg">
             Hi, My nickname is Books. I am a dedicated, hardworking, and
-            proactive Network Engineer. I am interested in <b>Networking</b>,
-            <b>Cybersecurity</b>, and <b>Data Analytics</b>. I constantly seek
-            new knowledge and actively develop new skils to improve myself. I
-            seek chalenging opportunities where I can fully use my skills for
-            the success of the organization.
+            proactive Network Engineer. I am interested in
+            <b>Networking</b>,<b>Cybersecurity</b>, and <b>Data Analytics</b>. I
+            constantly seek new knowledge and actively develop new skils to
+            improve myself. I seek chalenging opportunities where I can fully
+            use my skills for the success of the organization.
           </p>
         </div>
       </el-col>
@@ -124,26 +124,56 @@
       </el-col>
       <el-col :span="7">
         <div class="grid-content bg-purple"></div>
-      </el-col> -->
+      </el-col>-->
     </el-row>
     <el-row class="hidden-sm-and-up">
-      <!-- <el-col :span="24">
+      <el-col :span="24">
         <h1 class="header-xs">SIWANONT SITTINAM</h1>
         <h3 class="header-xs">Network Engineer</h3>
-        <div class="content-title">
+        <div class="content-title-xs">
           <p class="p-header-name-xs">
             Hi, My nickname is Books. I am a dedicated, hardworking, and
-            proactive Network Engineer. I am interested in <b>Networking</b>,
-            <b>Cybersecurity</b>, and <b>Data Analytics</b>. I constantly seek
-            new knowledge and actively develop new skils to improve myself. I
-            seek chalenging opportunities where I can fully use my skills for
-            the success of the organization.
+            proactive Network Engineer. I am interested in
+            <b>Networking</b>,<b>Cybersecurity</b>, and <b>Data Analytics</b>. I
+            constantly seek new knowledge and actively develop new skils to
+            improve myself. I seek chalenging opportunities where I can fully
+            use my skills for the success of the organization.
           </p>
         </div>
         <div id="profilepic">
           <img src="@/assets/images/profilepic.jpg" class="img-xs" />
         </div>
-      </el-col> -->
+        <div id="contact">
+          <div class="top-xs content content-xs">
+            <p class="p-xs">SIWANONT1@GMAIL.COM</p>
+            <img src="@/assets/icons/mail.png" class="ico-xs" />
+          </div>
+          <div class="content content-xs">
+            <p class="p-xs">(+66)92-686-8855</p>
+            <img src="@/assets/icons/tel.png" class="ico-xs" />
+          </div>
+          <div class="content content-xs">
+            <p class="p-xs">DECEMBER 05,1995</p>
+            <img src="@/assets/icons/calendar.png" class="ico-xs" />
+          </div>
+          <div class="content content-xs">
+            <p class="p-xs">www.siwanont.ml</p>
+            <img src="@/assets/icons/web.png" class="ico-xs" />
+          </div>
+          <div class="content content-xs">
+            <p class="p-xs">linkedin.com/in/siwanont</p>
+            <img src="@/assets/icons/in.png" class="ico-xs" />
+          </div>
+          <div class="content content-xs">
+            <p class="p-xs">github.com/booktay</p>
+            <img src="@/assets/icons/gh.png" class="ico-xs" />
+          </div>
+          <div class="content content-xs">
+            <p class="p-xs">Thai(Native)/English</p>
+            <img src="@/assets/icons/translate.png" class="ico-xs" />
+          </div>
+        </div>
+      </el-col>
     </el-row>
   </div>
 </template>
