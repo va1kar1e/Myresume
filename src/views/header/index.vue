f9cdb9c31ac976bacd4c2ced8ca0ee07d7afd601
Siwanont Sittinam <siwanont1@gmail.com>
2020-02-10 23:17:36 +0700

    Add contact

--- src/views/header/index.vue
+++ src/views/header/index.vue
@@ -1,37 +1,121 @@
 <template>
   <div id="header">
     <el-row class="hidden-lg-and-down">
-      <el-col :span="13" :justify="start">
-        <h1 class="header-name header-name-xl">SIWANONT SITTINAM</h1>
-        <h3>A Network Engineer</h3>
+      <el-col :span="12">
+        <h1 class="h1-header-name-xl">SIWANONT SITTINAM</h1>
+        <h3 class="h3-header-name-xl">Network Engineer</h3>
+        <div class="content-title">
+          <p class="p-header-name-xl">
+            Hi, My nickname is Books. I am a dedicated, hardworking, and
+            proactive Network Engineer. I am interested in <b>Networking</b>,
+            <b>Cybersecurity</b>, and <b>Data Analytics</b>. I constantly seek
+            new knowledge and actively develop new skils to improve myself. I
+            seek chalenging opportunities where I can fully use my skills for
+            the success of the organization.
+          </p>
+        </div>
       </el-col>
-      <el-col :span="4">
+      <el-col :span="6">
         <div id="profilepic">
           <img src="@/assets/images/profilepic.jpg" class="img-xl" />
         </div>
       </el-col>
-      <el-col :span="7">
-        <div class="grid-content bg-purple"></div>
+      <el-col :span="6" id="contact">
+        <div class="top-xl content content-xl">
+          <p class="p-xl">SIWANONT1@GMAIL.COM</p>
+          <img src="@/assets/icons/mail.png" class="ico-xl" />
+        </div>
+        <div class="content content-xl">
+          <p class="p-xl">(+66)92-686-8855</p>
+          <img src="@/assets/icons/tel.png" class="ico-xl" />
+        </div>
+        <div class="content content-xl">
+          <p class="p-xl">DECEMBER 05,1995</p>
+          <img src="@/assets/icons/calendar.png" class="ico-xl" />
+        </div>
+        <div class="content content-xl">
+          <p class="p-xl">www.siwanont.ml</p>
+          <img src="@/assets/icons/web.png" class="ico-xl" />
+        </div>
+        <div class="content content-xl">
+          <p class="p-xl">linkedin.com/in/siwanont</p>
+          <img src="@/assets/icons/in.png" class="ico-xl" />
+        </div>
+        <div class="content content-xl">
+          <p class="p-xl">github.com/booktay</p>
+          <img src="@/assets/icons/gh.png" class="ico-xl" />
+        </div>
+        <div class="content content-xl">
+          <p class="p-xl">Thai(Native)/English</p>
+          <img src="@/assets/icons/translate.png" class="ico-xl" />
+        </div>
       </el-col>
     </el-row>
     <el-row class="hidden-md-and-down hidden-xl-only">
-      <el-col :span="13" :justify="start">
-        <h1 class="header-name header-name-lg">SIWANONT SITTINAM</h1>
-        <h3>A Network Engineer</h3>
+      <el-col :span="12">
+        <h1 class="h1-header-name-lg">SIWANONT SITTINAM</h1>
+        <h3 class="h3-header-name-lg">Network Engineer</h3>
+        <div class="content-title">
+          <p class="p-header-name-lg">
+            Hi, My nickname is Books. I am a dedicated, hardworking, and
+            proactive Network Engineer. I am interested in <b>Networking</b>,
+            <b>Cybersecurity</b>, and <b>Data Analytics</b>. I constantly seek
+            new knowledge and actively develop new skils to improve myself. I
+            seek chalenging opportunities where I can fully use my skills for
+            the success of the organization.
+          </p>
+        </div>
       </el-col>
-      <el-col :span="4">
+      <el-col :span="6">
         <div id="profilepic">
           <img src="@/assets/images/profilepic.jpg" class="img-lg" />
         </div>
       </el-col>
-      <el-col :span="7">
-        <div class="grid-content bg-purple"></div>
+      <el-col :span="6" id="contact">
+        <div class="top-lg content content-lg">
+          <p class="p-lg">SIWANONT1@GMAIL.COM</p>
+          <img src="@/assets/icons/mail.png" class="ico-lg" />
+        </div>
+        <div class="content content-lg">
+          <p class="p-lg">(+66)92-686-8855</p>
+          <img src="@/assets/icons/tel.png" class="ico-lg" />
+        </div>
+        <div class="content content-lg">
+          <p class="p-lg">DECEMBER 05,1995</p>
+          <img src="@/assets/icons/calendar.png" class="ico-lg" />
+        </div>
+        <div class="content content-lg">
+          <p class="p-lg">www.siwanont.ml</p>
+          <img src="@/assets/icons/web.png" class="ico-lg" />
+        </div>
+        <div class="content content-lg">
+          <p class="p-lg">linkedin.com/in/siwanont</p>
+          <img src="@/assets/icons/in.png" class="ico-lg" />
+        </div>
+        <div class="content content-lg">
+          <p class="p-lg">github.com/booktay</p>
+          <img src="@/assets/icons/gh.png" class="ico-lg" />
+        </div>
+        <div class="content content-lg">
+          <p class="p-lg">Thai(Native)/English</p>
+          <img src="@/assets/icons/translate.png" class="ico-lg" />
+        </div>
       </el-col>
     </el-row>
     <el-row class="hidden-lg-and-up hidden-xs-only">
-      <el-col :span="13">
-        <h1 class="header-name header-name-md-sm">SIWANONT SITTINAM</h1>
-        <h3>B Network Engineer</h3>
+      <!-- <el-col :span="13">
+        <h1 class="h1-header-name-md-sm">SIWANONT SITTINAM</h1>
+        <h3 class="h3-header-name-md-sm">Network Engineer</h3>
+        <div class="content-title">
+          <p class="p-header-name-md-sm">
+            Hi, My nickname is Books. I am a dedicated, hardworking, and
+            proactive Network Engineer. I am interested in <b>Networking</b>,
+            <b>Cybersecurity</b>, and <b>Data Analytics</b>. I constantly seek
+            new knowledge and actively develop new skils to improve myself. I
+            seek chalenging opportunities where I can fully use my skills for
+            the success of the organization.
+          </p>
+        </div>
       </el-col>
       <el-col :span="4">
         <div id="profilepic">
@@ -40,16 +124,26 @@
       </el-col>
       <el-col :span="7">
         <div class="grid-content bg-purple"></div>
-      </el-col>
+      </el-col> -->
     </el-row>
     <el-row class="hidden-sm-and-up">
-      <el-col :span="24">
-        <h1 class="header-name header-name-xs">SIWANONT SITTINAM</h1>
-        <h3>C Network Engineer</h3>
+      <!-- <el-col :span="24">
+        <h1 class="header-xs">SIWANONT SITTINAM</h1>
+        <h3 class="header-xs">Network Engineer</h3>
+        <div class="content-title">
+          <p class="p-header-name-xs">
+            Hi, My nickname is Books. I am a dedicated, hardworking, and
+            proactive Network Engineer. I am interested in <b>Networking</b>,
+            <b>Cybersecurity</b>, and <b>Data Analytics</b>. I constantly seek
+            new knowledge and actively develop new skils to improve myself. I
+            seek chalenging opportunities where I can fully use my skills for
+            the success of the organization.
+          </p>
+        </div>
         <div id="profilepic">
           <img src="@/assets/images/profilepic.jpg" class="img-xs" />
         </div>
-      </el-col>
+      </el-col> -->
     </el-row>
   </div>
 </template>
