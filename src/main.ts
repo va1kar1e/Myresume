7173ac6127f6ea8e63bea37a046a60c153650dba
Siwanont Sittinam <siwanont1@gmail.com>
2020-02-16 22:31:20 +0700

    Add Vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "@/styles/index.scss";
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
 
@@ -10,5 +11,6 @@ new Vue({
   el: "#app",
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
