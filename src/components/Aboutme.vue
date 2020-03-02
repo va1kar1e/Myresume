3f1ba356cbade6b42a1d1273eee5471135cd6617
Siwanont Sittinam <siwanont1@gmail.com>
2020-03-03 00:00:44 +0700

    Add About Me

---
+++ src/components/Aboutme.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-col class="mb-1" cols="12">
+    <h2 class="display-1 font-weight-bold mb-1">
+      About Me
+    </h2>
+    <v-row justify="center">
+      <v-col cols="12">
+        <p class="title text-capitalize font-weight-light text-left text-md-justify">
+          Hi, My nickname is Books. I am a dedicated, hardworking, and proactive Network Engineer.
+          I am interested in Networking, Cybersecurity, and Data Analytics. I constantly seek new knowledge
+          and actively develop new skills to improve myself. I seek challenging opportunities where I can
+          fully use my skills for the success of the organization.
+        </p>
+      </v-col>
+    </v-row>
+  </v-col>
+</template>
+
+<style>
+p {
+  padding: 0px 5px 0px 20px;
+}
+</style>
+
+<script lang="ts">
+export default {
+  name: "Header"
+};
+</script>
