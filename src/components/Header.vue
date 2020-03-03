1803d1a686de5d64303010c4e87ebfb8a9677573
Siwanont Sittinam <siwanont1@gmail.com>
2020-03-04 00:10:27 +0700

    change progress color

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,7 +11,7 @@
         >
           <template v-slot:placeholder>
             <v-row class="fill-height ma-0" align="center" justify="center">
-              <v-progress-circular indeterminate color="grey lighten-5" />
+              <v-progress-circular indeterminate color="black" />
             </v-row>
           </template>
         </v-img>
