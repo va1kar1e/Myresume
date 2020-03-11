d3c02ed75ed67e298abaa1fb9467bed016fe920d
Siwanont Sittinam <siwanont1@gmail.com>
2020-03-11 18:10:19 +0700

    Add Content

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,8 +1,6 @@
 <template>
   <v-col class="mb-1" cols="12">
-    <h2 class="display-1 font-weight-bold mb-5">
-      Work Experience
-    </h2>
+    <h2 class="display-1 font-weight-bold mb-5">Work Experience</h2>
     <v-row justify="center">
       <v-col class="mb-1" cols="12">
         <v-timeline :dense="$vuetify.breakpoint.mdAndDown">
@@ -13,24 +11,30 @@
             :icon="item.icon"
           >
             <template v-slot:opposite>
-              <span
-                :class="`headline font-weight-regular ${item.color}--text`"
-                v-text="item.year"
-              ></span>
+              <span :class="`headline font-weight-regular ${item.color}--text`" v-text="item.year"></span>
             </template>
             <div class="py-4">
-              <h2
-                :class="`headline font-weight-light mb-4 ${item.color}--text`"
-              >
-                {{ item.title }}
-              </h2>
               <div class="title">
-                <p class="text-capitalize font-weight-light">
-                  {{ item.role[0] }}
-                </p>
-                <!-- <p :class="`text-capitalize font-weight-thin text-left ${item.color}--text`">
-                  Achievements/Tasks
-                </p> -->
+                <h2 :class="`headline font-weight-light mb-4 ${item.color}--text`">{{ item.title }}</h2>
+                <p class=".subtitle-2 text-capitalize font-weight-light">{{ item.role }}</p>
+              </div>
+              <div class="team mb-5">
+                <p :class="`text-capitalize font-weight-light text-left ${item.color}--text header-content`">Team</p>
+                <p
+                  :class="`body-1 font-weight-light text-left black--text content`"
+                >{{ item.team }}</p>
+              </div>
+              <div class="archiev mb-5">
+                <p
+                  :class="`text-capitalize font-weight-light text-left ${item.color}--text header-content`"
+                >Achievements/Tasks</p>
+                <ul>
+                  <li v-for="(task, i) in item.archiev" :key="i" :class="`content`">
+                    <p
+                      :class="`body-1 font-weight-light text-left black--text`"
+                    >{{ task.name }}</p>
+                  </li>
+                </ul>
               </div>
             </div>
           </v-timeline-item>
@@ -44,6 +48,20 @@
 col {
   padding: 0px 40px 0px 40px;
 }
+p {
+  padding: 0px 0px 0px 0px;
+  margin-top: 5px;
+  margin-bottom: 0px !important;
+}
+ul {
+  padding-left: 35px !important;
+}
+.header-content {
+  margin-left: 5px;
+}
+.content {
+  margin-left: 15px;
+}
 </style>
 
 <script lang="ts">
@@ -53,25 +71,60 @@ export default {
     items: [
       {
         color: "cyan",
-        year: "2019 - Present",
+        year: "June 2019 - Present",
         icon: "fas fa-server",
         title: "REFINITIV",
-        role: ["Technology Graduate Trainee - Network Engineer"]
+        role: "Technology Graduate Trainee",
+        team: "Data Center Operation Network Change - Network Engineer",
+        archiev: [
+          {
+            name: "LAN Implementation"
+          },
+          {
+            name: "TUMBOON : Money Donation Web Application"
+          }
+        ]
       },
       {
         color: "green",
         year: "March 2016 - May 2019",
         icon: "fas fa-search",
-        title:
-          "Massive Information and Knowledge Engineering Laboratory",
-        role: ["Junior Research Student"]
+        title: "Computer Engineering, Kasetsart University",
+        role: "Junior Research Student",
+        team: "Massive Information and Knowledge Engineering Laboratory",
+        archiev: [
+          {
+            name: "Social Network Analysis with Ego Network using Twiter Data"
+          },
+          {
+            name: "Real-Time Sentiment Change Detection of Twiter Data Stream"
+          },
+          {
+            name: "Linux System Administrator (Ubuntu,CentOS)"
+          },
+          {
+            name: "Teacher Assistant"
+          }
+        ]
       },
       {
         color: "purple",
         year: "June - August 2017",
         icon: "fas fa-code",
         title: "INTERNET MARKETING",
-        role: ["Ful-stack Web Developer, Data Engineer"]
+        role: "Outsource Fullstack Developer and Data Engineer",
+        team: "Data Science",
+        archiev: [
+          {
+            name: "Social Dashboard Monitoring"
+          },
+          {
+            name: "Social Influencer Analysis with Social Graph"
+          },
+          {
+            name: "Social Trend Analysis with Wordcloud"
+          }
+        ]
       }
     ]
   })
