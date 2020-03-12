6e92eb20e06c7235290bfd63e393d865f988f637
Siwanont Sittinam <siwanont1@gmail.com>
2020-03-13 00:06:16 +0700

    Add Content in Rec, Skill and Interest

--- src/components/Recognition.vue
+++ src/components/Recognition.vue
@@ -1,16 +1,40 @@
 <template>
-  <v-row justify="center">
-    <v-col class="mb-1" cols="12" md="6" lg="6">
-      <h2 class="display-1 font-weight-bold mb-5">Recognition</h2>
-    </v-col>
-  </v-row>
+  <v-col class="mb-1" cols="12" md="6" lg="6">
+    <h2 class="display-1 font-weight-bold mb-5">Recognition</h2>
+    <div v-for="(item, i) in items" :key="i" style="margin-bottom: 30px;">
+      <h2 :class="`headline font-weight-light mb-4 ${item.color}--text`">{{ item.what }}</h2>
+      <p class=".subtitle-2 text-capitalize font-weight-light">{{ item.where }}</p>
+      <p class=".subtitle-2 text-capitalize font-weight-light">{{ item.who }}</p>
+    </div>
+  </v-col>
 </template>
 
 <script lang="ts">
 export default {
   name: "Recognition",
   data: () => ({
-    dialog: false
+    dialog: false,
+    items: [
+      {
+        color: "blue",
+        what: "CONSOLATION PRIZE AWARD",
+        where: "Huawei ICT Completition 2019",
+        who: "Huawei Technologies (Thailand)"
+      },
+      {
+        color: "blue",
+        what: "CREATIVE DESIGN AWARD",
+        where: "Financial Cybersecurity 2017",
+        who: "Securities and Exchange Commission of Thailand"
+      }
+    ]
   })
 };
 </script>
+
+
+
+
+
+
+
---
+++ src/components/Section3.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row justify="center">
+    <Recognition />
+    <SkillInterest />
+  </v-row>
+</template>
+
+<script lang="ts">
+import Recognition from "@/components/Recognition.vue";
+import SkillInterest from "@/components/SkillInterest.vue";
+export default {
+  name: "Section3",
+  components: {
+    Recognition,
+    SkillInterest,
+  }
+};
+</script>
