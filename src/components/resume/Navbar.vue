fdf9c6274f6fcf9391cd36556afbd32b9a3548fc
Siwanont Sittinam <siwanont1@gmail.com>
2020-05-14 01:22:50 +0700

    Modified New UI

---
+++ src/components/resume/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app-bar app>
+    <div class="d-flex align-center">
+      <v-img
+        class="shrink mr-2"
+        contain
+        :src="require('@/assets/images/logo.png')"
+        transition="scale-transition"
+        width="40"
+      />
+      <v-toolbar-title class="shrink mt-1 font-weight-bold" contain>Siwanont's Resume</v-toolbar-title>
+    </div>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="d-none d-md-flex">
+      <v-btn to="/d/resume" target="_blank" text>
+        <span class="mr-2">Download My Resume</span>
+        <v-icon color="grey darken-4">fas fa-cloud-download-alt</v-icon>
+      </v-btn>
+      <v-btn href="mailto:siwanont1@gmail.com" text color="black" target="_blank">
+        <span class="mr-2">Hire me</span>
+        <v-icon color="grey darken-4">fas fa-paper-plane</v-icon>
+      </v-btn>
+    </v-toolbar-items>
+    <v-menu bottom close-on-click>
+      <template v-slot:activator="{ on }">
+        <v-btn dark icon v-on="on" class="d-flex d-md-none">
+          <v-icon color="grey darken-4">mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item to="/d/resume" target="_blank">
+          <v-list-item-icon>
+            <v-icon color="grey darken-4">fas fa-cloud-download-alt</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Download My Resume</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item to="/d/resume" target="_blank">
+          <v-list-item-icon>
+            <v-icon color="grey darken-4">fas fa-paper-plane</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Hire me</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
+</template>
