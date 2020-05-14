28783b718f825e661ccd2640c9a9178d44bd7d09
Siwanont Sittinam <siwanont1@gmail.com>
2020-05-14 22:58:10 +0700

    Add Part to Resume

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -6,6 +6,10 @@
       <v-container>
         <v-row class="text-center">
           <Aboutme />
+          <Education />
+          <Work />
+          <Extra />
+          <SkillInterest />
         </v-row>
       </v-container>
     </v-content>
@@ -18,6 +22,10 @@ import Navbar from "@/components/resume/Navbar.vue";
 import Footer from "@/components/resume/Footer.vue";
 import Header from "@/components/resume/Header.vue";
 import Aboutme from "@/components/resume/Aboutme.vue";
+import Work from "@/components/resume/Work.vue";
+import Education from "@/components/resume/Education.vue";
+import Extra from "@/components/resume/Extra.vue";
+import SkillInterest from "@/components/resume/SkillInterest.vue";
 
 export default {
   data() {
@@ -29,6 +37,10 @@ export default {
     Navbar,
     Header,
     Aboutme,
+    Work,
+    Education,
+    Extra,
+    SkillInterest,
     Footer
   }
 };
