75209351b68448daff0f42da908c038b0332d1fe
va1kar1e <siwanont1@gmail.com>
2020-07-21 03:25:01 +0700

    Add views

---
+++ src/views/Path.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-content>
+      <h1>Path</h1>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      title: "Path"
+    };
+  },
+  components: {
+  }
+};
+</script>
