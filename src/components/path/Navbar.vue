ac7a15cff9a21a8828e789c701d5ace4e78a93ca
va1kar1e <siwanont1@gmail.com>
2021-01-07 15:43:56 +0700

    Add In Construction to Path

---
+++ src/components/path/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app-bar app>
+    <div class="d-flex align-center">
+      <v-img
+        class="shrink mr-2"
+        contain
+        :src="require('@/assets/images/logo.png')"
+        transition="scale-transition"
+        width="40"
+      />
+      <v-toolbar-title class="shrink mt-1 font-weight-bold" contain
+        >Path</v-toolbar-title
+      >
+    </div>
+    <v-spacer></v-spacer>
+  </v-app-bar>
+</template>
+
+<style scoped>
+.v-app-bar {
+  background-color: #263859 !important;
+}
+.v-sheet {
+  background-color: #263859 !important;
+}
+</style>
+
+<script>
+
+export default {
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+  components: {
+  },
+};
+</script>
