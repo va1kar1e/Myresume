0b19589a33d5c8ec8cbac56cec90d02771123f5d
va1kar1e <siwanont1@gmail.com>
2021-01-07 15:38:38 +0700

    Refaceter Course and Interest Component

---
+++ src/components/profile/section3/courseInterest/CourseInterest.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-col cols="12" md="6" lg="6">
+    <Course />
+    <Interest />
+  </v-col>
+</template>
+
+<script lang="ts">
+import Course from "./components/Course.vue";
+import Interest from "./components/Interest.vue";
+
+export default {
+  name: "CourseInterest",
+  components: {
+    Course,
+    Interest
+  },
+};
+</script>
