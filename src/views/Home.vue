987634638526e91c8ab1e72de5e56b94ca557182
va1kar1e <siwanont1@gmail.com>
2021-01-07 11:49:06 +0700

    Add Home Path to Move Resume Page to Profile Page

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row class="text-center">
+          <h1>Home</h1>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      title: "Home"
+    };
+  },
+  components: {
+  }
+};
+</script>
