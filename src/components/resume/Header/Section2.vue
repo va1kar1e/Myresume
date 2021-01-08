86f8353dd00ca544a406f9c8f973c6fdc016858d
va1kar1e <siwanont1@gmail.com>
2021-01-08 19:58:01 +0700

    Add Resume Header Section2

--- src/components/resume/Header/Section2.vue
+++ src/components/resume/Header/Section2.vue
@@ -1,13 +1,26 @@
 <template>
-  <v-col cols="3" class="green">
-    <h4 class="display-2 font-weight-bold text-uppercase text-center">
-      Header Section2
-    </h4>
+  <v-col cols="3" class="resume-header-section2">
+    <v-img
+      :src="require('@/assets/images/profilepic.png')"
+      class="resume-header-section-img"
+      contain
+      min-height="200"
+    >
+      <template v-slot:placeholder>
+        <v-row class="fill-height ma-0" align="center" justify="center">
+          <v-progress-circular indeterminate color="black" />
+        </v-row>
+      </template>
+    </v-img>
   </v-col>
 </template>
 
 <script>
 export default {
-  
-}
+  name: "ResumeHeaderSection2",
+  data() {
+    return {
+    };
+  }
+};
 </script>
