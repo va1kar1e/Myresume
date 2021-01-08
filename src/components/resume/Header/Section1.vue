c801f9e609df0200b41dace45136749c61e7ed32
va1kar1e <siwanont1@gmail.com>
2021-01-08 19:57:49 +0700

    Add Resume Header Section1

--- src/components/resume/Header/Section1.vue
+++ src/components/resume/Header/Section1.vue
@@ -1,10 +1,18 @@
 <template>
-  <v-col cols="5" class="red">
+  <v-col cols="6" class="resume-header-section1">
     <h4 class="headline font-weight-bold text-uppercase">
       {{fullname}}
     </h4>
     <p class="caption font-weight-bold text-uppercase">
       {{title}}
+    </p>
+    <p
+      v-for="(content, i) in aboutme"
+      :key="i"
+      class="caption text-center text-md-justify resume-header-section-p"
+      v-html="content"
+    >
+      {{ content }}
     </p>
   </v-col>
 </template>
@@ -17,7 +25,8 @@ export default {
   data() {
     return {
       fullname: info.header.fullname,
-      title: info.header.title
+      title: info.header.title,
+      aboutme: info.aboutme.contentText,
     };
   }
 };
