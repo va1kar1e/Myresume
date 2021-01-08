7dd3f01ef4e5e3170da19b4b77cda54d9f31a125
va1kar1e <siwanont1@gmail.com>
2021-01-09 00:51:49 +0700

    Remove Line Component

--- src/components/resume/Header.vue
+++ src/components/resume/Header.vue
@@ -5,12 +5,11 @@
       <Section2 />
       <Section3 />
     </v-row>
-    <HorizontalLine />
+    <div class="horizontal-line"></div>
   </div>
 </template>
 
 <script>
-import HorizontalLine from "@/components/resume/Components/HorizontalLine.vue";
 import Section1 from "./Header/Section1.vue"
 import Section2 from "./Header/Section2.vue"
 import Section3 from "./Header/Section3.vue"
@@ -18,7 +17,6 @@ import Section3 from "./Header/Section3.vue"
 export default {
   name: "ResumeHeader",
   components: {
-    HorizontalLine,
     Section1,
     Section2,
     Section3,
--- src/components/resume/components/HorizontalLine.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <v-row class="d-flex text-center">
-    <v-col cols="12">
-      <div class="horizontal-line"></div>
-    </v-col>
-  </v-row>
-</template>
