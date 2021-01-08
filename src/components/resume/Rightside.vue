c7458bd62b4322d77ac933383f8f8c552c84c41f
va1kar1e <siwanont1@gmail.com>
2021-01-09 00:52:09 +0700

    Add Cert to Rightside

--- src/components/resume/Rightside.vue
+++ src/components/resume/Rightside.vue
@@ -1,16 +1,19 @@
 <template>
   <v-col cols="6" class="text-left resume-right">
     <Education />
+    <Cert />
   </v-col>
 </template>
 
 <script>
 import Education from "./Rightside/Education.vue";
+import Cert from "./Rightside/Cert.vue";
 
 export default {
   name: "ResumeLeftSide",
   components: {
     Education,
+    Cert,
   }
 };
 </script>
