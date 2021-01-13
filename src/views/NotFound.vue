1ce355ee2a03e375e5a682a17d6ac14ae4603d38
Valkarie <siwanont1@gmail.com>
2021-01-13 21:44:25 +0700

    Update NotFound.vue

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -5,16 +5,21 @@
         <v-layout row wrap>
           <v-flex xs12>
             <v-card height="400" color="transparent" flat>
-              <div class="display-3 mt-5">Page not found.</div>
-              <div class="grey--text lighten-5">
+              <div class="display-3 mt-5">This page not found.</div>
+              <div class="grey--text title mt-2">
                 The page you are trying to get to never existed in this reality,
                 or has migrated to a parallel universe.
               </div>
-              <div class="paragraph-text mt-2">
+              <div class="headline mt-2 text-center">
                 <br />
-                <br />Try going back to home page and repeating your action. Or,
-                contact helpdesk for support.
+                Try going back to <a href="/" class="text-uppercase">Home page</a>
               </div>
+              <br />
+              <div class="horizontal-line" style="border-color:white;"></div>
+              <br />
+              <p class="title font-weight-light text-center">
+                Copyright@{{ new Date().getFullYear() }}, Designed by Siwanont Sittinam with <a href="https://vuejs.org/">Vue.JS</a>
+              </p>
             </v-card>
           </v-flex>
         </v-layout>
