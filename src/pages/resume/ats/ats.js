28b15ff357ff7baedef1800568c93aae44ac667d
Valkarie <siwanont1@gmail.com>
2021-04-08 23:52:16 +0700

    Create Resume
    General and ATS

---
+++ src/pages/resume/ats/ats.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+
+function ResumeATS() {
+  return <Button color="primary">Resume ATS</Button>;
+}
+
+export default ResumeATS;
---
+++ src/pages/resume/general/general.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+
+function ResumeGeneral() {
+  return <Button color="primary">Resume General</Button>;
+}
+
+export default ResumeGeneral;
