f2f22c24b25ca28434350c78203f581f79205f5f
Valkarie <siwanont1@gmail.com>
2021-04-09 01:01:03 +0700

    Update profileimages.js
    Add Height

--- src/pages/profile/components/profileimages.js
+++ src/pages/profile/components/profileimages.js
@@ -1,11 +1,9 @@
 import Profile from "@images/profile.jpg";
 
-function ProfileImage() {
-  return (
-    <div className="ProfileImage">
-      <img src={Profile} className="App-logo" alt="logo" />
-    </div>
-  );
-}
+const ProfileImage = () => (
+  <div className="ProfileImage">
+    <img src={Profile} className="App-logo" alt="logo" height="300" />
+  </div>
+);
 
 export default ProfileImage;
