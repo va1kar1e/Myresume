abf8c8011ec1df7efa408423f9e0f1ad1470d03c
Valkarie <siwanont1@gmail.com>
2021-04-08 23:51:58 +0700

    Create dashboard.js

---
+++ src/pages/dashboard/dashboard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+
+function Dashboard() {
+  return <Button color="primary">Dashboard</Button>;
+}
+
+export default Dashboard;
