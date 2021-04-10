c92aba1c6a2bbaf1b697665aa23e0ae9be6fd58e
Valkarie <siwanont1@gmail.com>
2021-04-11 02:29:20 +0700

    Update dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,7 +1,31 @@
 import React from "react";
+import Nav from "@components/nav";
 
-function Dashboard() {
-  return <h1>Dashboard</h1>;
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.setState({});
+  }
+
+  render() {
+    return (
+      <section className="hero is-fullheight">
+        <div className="hero-head">
+          <Nav />
+        </div>
+        <div className="hero-body">
+          <div className="container">
+            <p className="has-text-white">Dashboard</p>
+          </div>
+        </div>
+        <div className="hero-foot"></div>
+      </section>
+    );
+  }
 }
 
 export default Dashboard;
