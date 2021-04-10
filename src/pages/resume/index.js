f5a7fff65800bf3d45988b3cbbd8341663569e07
Valkarie <siwanont1@gmail.com>
2021-04-11 02:29:00 +0700

    Update index.js

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -1,28 +1,26 @@
-import React, { Fragment } from "react";
-import { Route, Switch, Link } from "react-router-dom";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import ResumeGeneral from "./general";
 import ResumeATS from "./ats";
 
-function Resume() {
-  return (
-    <Fragment>
-      <h1>Resume</h1>
-      <Link to="/" className="navbar-item">
-        Home
-      </Link>
-      <Link to="/resume/general" className="navbar-item">
-        General
-      </Link>
-      <Link to="/resume/ats" className="navbar-item">
-        ATS
-      </Link>
+class Resume extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  componentDidMount() {
+    this.setState({});
+  }
+
+  render() {
+    return (
       <Switch>
         <Route path="/resume/general" component={ResumeGeneral} />
         <Route path="/resume/ats" component={ResumeATS} />
       </Switch>
-    </Fragment>
-  );
+    );
+  }
 }
 
 export default Resume;
