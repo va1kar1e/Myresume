29f825e923cf913cf604a78d7903b64e83a0d764
Valkarie <siwanont1@gmail.com>
2021-04-11 02:01:25 +0700

    Update index.js

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,11 +1,9 @@
 import { React, Fragment } from "react";
-import ProfileImage from "./components/profileimages";
 
 function Profile() {
   return (
     <Fragment>
       <h1>Profile</h1>
-      <ProfileImage />
     </Fragment>
   );
 }
