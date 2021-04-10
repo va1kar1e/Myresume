50b0cf1f9e3e440a2759ebd8bc11ac89cb7b94bf
Valkarie <siwanont1@gmail.com>
2021-04-10 19:59:34 +0700

    Update link

--- src/pages/link/index.js
+++ src/pages/link/index.js
@@ -1,28 +1,44 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
+import mydata from "@mydata";
 
-function Link() {
-  return (
-    <Fragment>
-      <h1>Link</h1>
+class Link extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      linkedin: "",
+      github: "",
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      linkedin: mydata["profile"]["link"]["linkedin"],
+      github: mydata["profile"]["link"]["github"],
+    });
+  }
+
+  render() {
+    var { linkedin, github } = this.state;
+    return (
       <Switch>
         <Route
           path="/link/github"
           component={() => {
-            window.location.href = "https://github.com";
+            window.location.href = "https://github.com/" + github["username"];
             return null;
           }}
         />
         <Route
           path="/link/linkedin"
           component={() => {
-            window.location.href = "https://linkedin.com";
+            window.location.href =
+              "https://linkedin.com/in/" + linkedin["username"];
             return null;
           }}
         />
       </Switch>
-    </Fragment>
-  );
+    );
+  }
 }
-
 export default Link;
