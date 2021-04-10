005bda9b7b21b20059b2833312746e77c7264003
Valkarie <siwanont1@gmail.com>
2021-04-10 19:58:55 +0700

    Update index.js

--- src/pages/notfound/index.js
+++ src/pages/notfound/index.js
@@ -20,10 +20,10 @@ class Notfound extends React.Component {
     return (
       <div className="container">
         <div className="section is-large">
-          <p className="title is-1 is-size-1-tablet is-size-2-mobile is-uppercase has-text-weight-bold">
+          <p className="title is-size-1-tablet is-size-2-mobile is-uppercase has-text-weight-bold has-text-primary">
             404 not found
           </p>
-          <p className="subtitle is-3 is-size-3-tablet is-size-4-mobile">
+          <p className="subtitle is-size-3-tablet is-size-4-mobile has-text-white">
             The page you are trying to get to never existed in this reality, or
             has migrated to a parallel universe. Click this link to return to{" "}
             <strong>
