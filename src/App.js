bed70599561bc20e7f281d77d5b67176545e98c3
Valkarie <siwanont1@gmail.com>
2021-04-10 19:59:19 +0700

    Init  fontawesome

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import "./App.scss";
+import "@fortawesome/fontawesome-free/js/brands.js";
+import "@fortawesome/fontawesome-free/js/solid.js";
+import "@fortawesome/fontawesome-free/js/fontawesome.js";
 
 import Router from "./router";
 
