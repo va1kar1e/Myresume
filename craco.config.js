3d780b2002a929f2e8b7f226f9936c4fe51724c4
Valkarie <siwanont1@gmail.com>
2021-04-11 02:00:36 +0700

    Alias component

--- craco.config.js
+++ craco.config.js
@@ -3,6 +3,7 @@ module.exports = {
   webpack: {
     alias: {
       "@pages": path.resolve(__dirname, "src/pages/"),
+      "@components": path.resolve(__dirname, "src/components/"),
       "@mydata": path.resolve(__dirname, "src/assets/data.json"),
       "@images": path.resolve(__dirname, "src/assets/images/"),
     },
