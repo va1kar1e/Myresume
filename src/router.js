6a8b735bfb1d9556a4d6faff0d9d6a29348a7b44
Valkarie <siwanont1@gmail.com>
2021-04-11 22:04:00 +0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -5,6 +5,8 @@ import { Route, Switch } from "react-router-dom";
 import Home from "@pages/home";
 import Profile from "@pages/profile";
 import Dashboard from "@pages/dashboard";
+import ResumeGeneral from "@pages/resume/general";
+import ResumeATS from "@pages/resume/ats";
 import Link from "@pages/link";
 import NotFound from "./pages/notfound";
 
@@ -16,6 +18,8 @@ function Router() {
         <Route path="/profile" component={Profile} />
         <Route path="/link" component={Link} />
         <Route path="/dashboard" component={Dashboard} />
+        <Route path="/resume/general" component={ResumeGeneral} />
+        <Route path="/resume/ats" component={ResumeATS} />
         <Route component={NotFound} />
       </Switch>
     </div>
