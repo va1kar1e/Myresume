0916c4d121506402a0d604a79bc702eea66a3126
Valkarie <siwanont1@gmail.com>
2021-04-11 07:56:34 +0700

    Update cert.js

--- src/pages/profile/components/cert.js
+++ src/pages/profile/components/cert.js
@@ -1,33 +1,50 @@
 import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 import mydata from "@mydata";
 
 class ProfileCert extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      course: "",
+      cert: "",
     };
   }
 
   componentDidMount() {
     this.setState({
-      course: mydata["profile"]["course"],
+      cert: mydata["profile"]["cert"],
     });
   }
 
   render() {
-    var { course } = this.state;
+    var { cert } = this.state;
     return (
       <Fragment>
         <div className="content">
           <p className="title has-text-primary">Certificates</p>
         </div>
         <div className="tags are-medium">
-          {course ? (
-            course.map((c, index) => (
-              <span key={index} class="tag is-primary">
-                {c}
-              </span>
+          {cert ? (
+            cert.map((c, index) => (
+              <div class="card">
+                <div class="card-image">
+                  <figure className="image is-square">
+                    <img
+                      src="https://bulma.io/images/placeholders/256x256.png"
+                      alt=""
+                    />
+                  </figure>
+                </div>
+                <div className="card-content">
+                  <div className="media">
+                    <div class="media-content">
+                      <Link to={c["id"]} className="is-6 has-text-centered">
+                        {c["abb"]}
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
             ))
           ) : (
             <p>Loading...</p>
