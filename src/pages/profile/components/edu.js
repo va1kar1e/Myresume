5cebc412bc4e18d3b28ebbc216326e8ad15bae35
Valkarie <siwanont1@gmail.com>
2021-04-11 20:24:43 +0700

    Update edu.js

--- src/pages/profile/components/edu.js
+++ src/pages/profile/components/edu.js
@@ -32,37 +32,32 @@ class ProfileEdu extends React.Component {
         </div>
         <div className="tile is-ancestor">
           <div className="tile is-parent is-vertical">
-            <article className="tile is-child notification is-primary">
-              <h1 className="title is-size-3-tablet is-size-4-mobile has-text-white is-uppercase">
+            <article className="tile is-child notification is-white">
+              <h1 className="title is-size-3-tablet is-size-4-mobile has-text-primary is-uppercase">
                 {general["university"]}
               </h1>
-              <p className="subtitle is-size-5-tablet is-size-6-mobile has-text-white is-uppercase has-text-weight-bold">
+              <p className="subtitle is-size-5-tablet is-size-6-mobile has-text-grey-dark is-uppercase has-text-weight-bold">
                 {general["faculty"]}
                 <br />
-                <small className="subtitle is-size-6-tablet is-size-7-mobile has-text-white">
+                <small className="subtitle is-size-6-tablet is-size-7-mobile has-text-grey-dark">
                   Period: {general["period"]}
                 </small>
               </p>
             </article>
-            <article className="tile is-child notification has-background-warning">
-              <h1 className="title is-size-3-tablet is-size-4-mobile has-text-white is-uppercase">
-                Internship
-              </h1>
-              <p className="subtitle is-size-5-tablet is-size-6-mobile has-text-white is-uppercase has-text-weight-bold">
-                {internship["university"]}
-                <br />
-                {internship["laboratory"]}
+            <article className="tile is-child notification is-white">
+              <p className="title is-size-4-tablet is-size-5-mobile has-text-primary is-uppercase">
+                Thesis
               </p>
               <div className="content">
-                <p className="is-size-6-tablet is-size-7-mobile has-text-white is-capitalized has-text-weight-bold">
-                  {internship["project"]}
+                <p className="is-size-6-tablet is-size-7-mobile has-text-grey-dark is-capitalized has-text-weight-bold">
+                  {thesis["name"]}
                 </p>
                 <ol>
-                  {internship ? (
-                    internship["archievement"].map((arch, index) => (
+                  {thesis ? (
+                    thesis["archievement"].map((arch, index) => (
                       <li
                         key={index}
-                        className="is-size-6-tablet is-size-7-mobile has-text-white has-text-weight-semibold"
+                        className="is-size-6-tablet is-size-7-mobile has-text-grey-dark has-text-weight-semibold"
                       >
                         {arch}
                       </li>
@@ -73,29 +68,60 @@ class ProfileEdu extends React.Component {
                 </ol>
               </div>
             </article>
-            <article className="tile is-child notification has-background-danger-dark">
-              <p className="title is-size-3-tablet is-size-4-mobile has-text-white is-uppercase">
+            <article className="tile is-child notification is-white">
+              <h1 className="title is-size-4-tablet is-size-5-mobile has-text-primary is-uppercase">
+                Internship
+              </h1>
+              <p className="subtitle is-size-6-tablet is-size-7-mobile has-text-grey-dark is-uppercase has-text-weight-bold">
+                {internship["university"]}
+                <br />
+                {internship["laboratory"]}
+              </p>
+              <div className="content">
+                <p className="is-size-6-tablet is-size-7-mobile has-text-grey-dark is-capitalized has-text-weight-bold">
+                  {internship["project"]}
+                </p>
+                <ol>
+                  {internship ? (
+                    internship["archievement"].map((arch, index) => (
+                      <li
+                        key={index}
+                        className="is-size-6-tablet is-size-7-mobile has-text-grey-dark has-text-weight-semibold"
+                      >
+                        {arch}
+                      </li>
+                    ))
+                  ) : (
+                    <p>Loading...</p>
+                  )}
+                </ol>
+              </div>
+            </article>
+          </div>
+          <div className="tile is-parent is-vertical">
+            <article className="tile is-child notification is-white">
+              <p className="title is-size-4-tablet is-size-5-mobile has-text-primary is-uppercase">
                 Work
               </p>
               {work ? (
                 work.map((w, index) => (
                   <section key={index}>
                     <div className="content">
-                      <p className="subtitle is-size-6-tablet is-size-7-mobile has-text-white is-capitalized has-text-weight-bold">
+                      <p className="subtitle is-size-6-tablet is-size-7-mobile has-text-grey-dark is-capitalized has-text-weight-bold">
                         {w["where"]}
                       </p>
                       <ul>
                         {w["do"].map((wdo, index) => (
                           <li
                             key={index}
-                            className="is-size-6-tablet is-size-7-mobile has-text-white has-text-weight-semibold"
+                            className="is-size-6-tablet is-size-7-mobile has-text-grey-dark has-text-weight-semibold"
                           >
                             {wdo["name"]}
                             <ol>
                               {wdo["archievement"].map((arch, index) => (
                                 <li
                                   key={index}
-                                  className="is-size-6-tablet is-size-7-mobile has-text-white has-text-weight-semibold"
+                                  className="is-size-6-tablet is-size-7-mobile has-text-grey-dark has-text-weight-semibold"
                                 >
                                   {arch}
                                 </li>
@@ -111,48 +137,22 @@ class ProfileEdu extends React.Component {
                 <p>Loading...</p>
               )}
             </article>
-          </div>
-          <div className="tile is-parent is-vertical">
-            <article className="tile is-child notification has-background-info">
-              <p className="title is-size-3-tablet is-size-4-mobile has-text-white is-uppercase">
-                Thesis
-              </p>
-              <div className="content">
-                <p className="is-size-6-tablet is-size-7-mobile has-text-white is-capitalized has-text-weight-bold">
-                  {thesis["name"]}
-                </p>
-                <ol>
-                  {thesis ? (
-                    thesis["archievement"].map((arch, index) => (
-                      <li
-                        key={index}
-                        className="is-size-6-tablet is-size-7-mobile has-text-white has-text-weight-semibold"
-                      >
-                        {arch}
-                      </li>
-                    ))
-                  ) : (
-                    <p>Loading...</p>
-                  )}
-                </ol>
-              </div>
-            </article>
-            <article className="tile is-child notification has-background-link-dark">
-              <p className="title is-size-3-tablet is-size-4-mobile has-text-white is-uppercase">
+            <article className="tile is-child notification is-white">
+              <p className="title is-size-4-tablet is-size-5-mobile has-text-primary is-uppercase">
                 Project
               </p>
               {project ? (
                 project.map((p, index) => (
                   <section key={index}>
                     <div className="content">
-                      <p className="subtitle is-size-6-tablet is-size-7-mobile has-text-white has-text-weight-bold">
+                      <p className="subtitle is-size-6-tablet is-size-7-mobile has-text-grey-dark has-text-weight-bold">
                         {p["name"]}
                       </p>
                       <ol>
                         {p["archievement"].map((arch, index) => (
                           <li
                             key={index}
-                            className="is-size-6-tablet is-size-7-mobile has-text-white has-text-weight-semibold"
+                            className="is-size-6-tablet is-size-7-mobile has-text-grey-dark has-text-weight-semibold"
                           >
                             {arch}
                           </li>
