59b4c024dd3d9d1794798036aad10f37297044e4
Valkarie <siwanont1@gmail.com>
2021-04-11 07:17:10 +0700

    Add Recognition

--- src/pages/profile/components/body.js
+++ src/pages/profile/components/body.js
@@ -1,76 +1,24 @@
 import React from "react";
-import mydata from "@mydata";
+import ProfileWork from "./work";
+import ProfileEdu from "./edu";
+import ProfileRecognition from "./recognition";
 
 class ProfileBody extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      works: "",
-    };
+    this.state = {};
   }
 
   componentDidMount() {
-    this.setState({
-      works: mydata["profile"]["work"],
-    });
+    this.setState({});
   }
 
   render() {
-    var { works } = this.state;
     return (
       <div className="container has-text-left">
-        <div className="content">
-          <p className="title has-text-primary">Works</p>
-        </div>
-        {works ? (
-          works.map((work, index) => (
-            <article className="is-white media" key={index}>
-              <figure className="media-left" style={{ color: work["color"] }}>
-                <span className="icon is-large">
-                  <i className={work["icon"] + " fa-2x"}></i>
-                </span>
-              </figure>
-              <div className="media-content">
-                <div className="content">
-                  <p>
-                    <strong
-                      className="is-capitalized"
-                      style={{ color: work["color"] }}
-                    >
-                      {work["role"]}
-                    </strong>{" "}
-                    <medium
-                      className="is-uppercase"
-                      style={{ color: work["color"] }}
-                    >
-                      @{work["name"]}
-                    </medium>{" "}
-                    <small> Period {work["year"]}</small>
-                    <br />
-                    {work["keyword"]}
-                  </p>
-                </div>
-                <article className="media">
-                  <div className="content">
-                    <p style={{ color: work["color"] }}>Team: {work["team"]}</p>
-                  </div>
-                </article>
-                <article className="media has-text-grey-darker">
-                  <div className="content">
-                    <p className="has-text-weight-bold">Archievement</p>
-                    <ol>
-                      {work["archiev"].map((arch, index) => (
-                        <li>{arch}</li>
-                      ))}
-                    </ol>
-                  </div>
-                </article>
-              </div>
-            </article>
-          ))
-        ) : (
-          <div className="is-loading">Loading...</div>
-        )}
+        <ProfileWork />
+        <ProfileEdu />
+        <ProfileRecognition />
       </div>
     );
   }
---
+++ src/pages/profile/components/recognition.js
@@ -0,0 +1,48 @@
+import React, { Fragment } from "react";
+import mydata from "@mydata";
+
+class ProfileRecognition extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recognition: "",
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      recognition: mydata["profile"]["recognition"],
+    });
+  }
+
+  render() {
+    var { recognition } = this.state;
+    return (
+      <Fragment>
+        <div className="content">
+          <p className="title has-text-primary">Recognition</p>
+        </div>
+        <div className="tile is-ancestor">
+          {recognition ? (
+            recognition.map((r, index) => (
+              <div class="tile is-parent">
+                <article className="tile is-child notification">
+                  <p className="title is-uppercase">{r["what"]}</p>
+                  <p className="subtitle">
+                    <strong className="is-italic">{r["when"]}</strong>
+                    <br />
+                    <p className="">{r["who"]}</p>
+                  </p>
+                </article>
+              </div>
+            ))
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default ProfileRecognition;
