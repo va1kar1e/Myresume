7a3a866187502e9ea9de8dd8a0eb30befaa6474a
Valkarie <siwanont1@gmail.com>
2021-04-11 07:36:09 +0700

    Add Course & Interest

---
+++ src/pages/profile/components/interest.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from "react";
+import mydata from "@mydata";
+
+class ProfileInterest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      interest: "",
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      interest: mydata["profile"]["interest"],
+    });
+  }
+
+  render() {
+    var { interest } = this.state;
+    return (
+      <Fragment>
+        <div className="content">
+          <p className="title has-text-primary">Interests</p>
+        </div>
+        <div className="tags are-medium">
+          {interest ? (
+            interest.map((i, index) => (
+              <span key={index} class="tag is-primary">
+                {i}
+              </span>
+            ))
+          ) : (
+            <p>Loading...</p>
+          )}
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default ProfileInterest;
