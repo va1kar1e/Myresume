11d1bd0c0817862cf7dcdb5c78f0c9b0556c8c95
Valkarie <siwanont1@gmail.com>
2021-04-12 23:25:12 +0700

    Add QR

---
+++ src/pages/resume/components/qrcode.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import QRCode from "qrcode.react";
+import logo from "@images/logo.png";
+
+class ResumeQRCode extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.setState({});
+  }
+
+  render() {
+    const { link, size } = this.props;
+    return (
+      <QRCode
+        value={link}
+        size={size}
+        renderAs="svg"
+        bgColor="#ffffff"
+        fgColor="#222629"
+        level="Q"
+        imageSettings={{
+          src: logo,
+          x: null,
+          y: null,
+          height: 30,
+          width: 30,
+          excavate: true,
+        }}
+      />
+    );
+  }
+}
+
+export default ResumeQRCode;
--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ResumeQRCode from "./components/qrcode";
+import logo from "@images/logo.png";
 
 class Resume extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      ats_path: "resume/Linkts",
+      general_path: "/resume/general",
+    };
   }
 
   componentDidMount() {
@@ -12,12 +17,70 @@ class Resume extends React.Component {
   }
 
   render() {
+    const { general_path, ats_path } = this.state;
     return (
       <section className="hero is-large">
         <div className="container">
-          <p className="has-text-white">Resume</p>
-          <Link to="/resume/general">General Version</Link>
-          <Link to="/resume/ats">ATS Version</Link>
+          <div class="columns">
+            <div class="column">
+              <div className="card">
+                <div className="card-image is-centered">
+                  <figure class="image is-128x128">
+                    <ResumeQRCode
+                      link={window.location.href + general_path}
+                      size={128}
+                    />
+                  </figure>
+                </div>
+                <div className="card-content">
+                  <div className="content">
+                    <p>General Version</p>
+                  </div>
+                </div>
+                <footer class="card-footer">
+                  <p class="card-footer-item">
+                    <span>
+                      View on <Link to={general_path}>Web</Link>
+                    </span>
+                  </p>
+                  <p class="card-footer-item">
+                    <span>
+                      <Link to="/">Download File</Link>
+                    </span>
+                  </p>
+                </footer>
+              </div>
+            </div>
+            <div class="column">
+              <div className="card">
+                <div className="card-image is-centered">
+                  <figure class="image is-128x128">
+                    <ResumeQRCode
+                      link={window.location.href + ats_path}
+                      size={128}
+                    />
+                  </figure>
+                </div>
+                <div className="card-content">
+                  <div className="content">
+                    <p>ATS Version</p>
+                  </div>
+                </div>
+                <footer class="card-footer">
+                  <p class="card-footer-item">
+                    <span>
+                      View on <Link to={ats_path}>Web</Link>
+                    </span>
+                  </p>
+                  <p class="card-footer-item">
+                    <span>
+                      <Link to="/">Download File</Link>
+                    </span>
+                  </p>
+                </footer>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     );
