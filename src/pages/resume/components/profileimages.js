7504433507c0555ddc21da58f97518baa25716e2
Valkarie <siwanont1@gmail.com>
2021-04-15 10:43:46 +0700

    Update profileimages.js

--- src/pages/resume/components/profileimages.js
+++ src/pages/resume/components/profileimages.js
@@ -2,7 +2,7 @@ import Profile from "@images/profile.jpg";
 
 const ProfileImage = () => (
   <div className="ProfileImage">
-    <img src={Profile} className="App-logo" alt="logo" height="300" />
+    <img src={Profile} className="App-logo" alt="logo" height="100" />
   </div>
 );
 
