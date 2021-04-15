4affa1a80cbdc26546cbf32809ff6b4e321488a7
Valkarie <siwanont1@gmail.com>
2021-04-16 00:46:19 +0700

    rewrite link path to param

--- src/pages/link/index.js
+++ src/pages/link/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
 import mydata from "@mydata";
 
 class Link extends React.Component {
@@ -15,47 +14,43 @@ class Link extends React.Component {
 
   componentDidMount() {
     this.setState({
-      linkedin: mydata["profile"]["link"]["linkedin"],
-      github: mydata["profile"]["link"]["github"],
+      linkedin: mydata["profile"]["link"]["linkedin"]["username"],
+      github: mydata["profile"]["link"]["github"]["username"],
       email: mydata["profile"]["aboutme"]["email"],
-      hassos: mydata["profile"]["link"]["hassos"],
+      hassos: mydata["profile"]["link"]["hassos"]["url"],
     });
   }
 
+  gotolink() {
+    const { type } = this.props.match.params;
+    const { linkedin, github, email, hassos } = this.state;
+    if (type === "github") {
+      window.location.href = "https://github.com/" + github;
+      return null;
+    } else if (type === "linkedin") {
+      window.location.href = "https://linkedin.com/in/" + linkedin;
+      return null;
+    } else if (type === "contact") {
+      window.location.href = "mailto:" + email;
+      return null;
+    } else if (type === "hassos") {
+      window.location.href = hassos;
+      return null;
+    } else {
+    }
+  }
+
   render() {
-    var { linkedin, github, email, hassos } = this.state;
+    this.gotolink();
+
     return (
-      <Switch>
-        <Route
-          path="/link/github"
-          component={() => {
-            window.location.href = "https://github.com/" + github["username"];
-            return null;
-          }}
-        />
-        <Route
-          path="/link/linkedin"
-          component={() => {
-            window.location.href =
-              "https://linkedin.com/in/" + linkedin["username"];
-            return null;
-          }}
-        />
-        <Route
-          path="/link/contact"
-          component={() => {
-            window.location.href = "mailto:" + email;
-            return null;
-          }}
-        />
-        <Route
-          path="/link/hassos"
-          component={() => {
-            window.location.href = "http://" + hassos["url"];
-            return null;
-          }}
-        />
-      </Switch>
+      <div className="container">
+        <div className="section is-large">
+          <p className="title is-size-1-tablet is-size-2-mobile is-uppercase has-text-weight-bold has-text-primary">
+            Redirect to External Link
+          </p>
+        </div>
+      </div>
     );
   }
 }
